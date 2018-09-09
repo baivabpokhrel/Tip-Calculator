0339c6346a8ef4da3ee6447ea9313bb96a77e3fe
Binod Pokhrel <Binod@binods-macbook-pro.local>
2018-09-09 01:55:22 -0400

    Files Uploaded

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var customerBillAmount: UITextField!
+   
+    
+    @IBOutlet weak var tipAmount: UILabel!
+    
+    @IBOutlet weak var totalBillAmount: UILabel!
+    
+    
     override func viewDidLoad() {
+        self.title = "Tip Calculator"
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tenPercentTip(_ sender: Any) {
+        let totalBillAmount=Double(self.customerBillAmount.text ?? "") ?? 0
+        let tipPercent=0.10
+        let tipAmount = totalBillAmount * tipPercent
+        self.tipAmount.text = "$\(tipAmount)"
+        let totalAmount = totalBillAmount + tipAmount
+        self.totalBillAmount.text = "$\(totalAmount)"
+        
+        }
+    
+    @IBAction func fifteenPercentTip(_ sender: Any) {
+        let totalBillAmount=Double(self.customerBillAmount.text ?? "") ?? 0
+        let tipPercent=0.15
+        let tipAmount = totalBillAmount * tipPercent
+        self.tipAmount.text = "$\(tipAmount)"
+        let totalAmount = totalBillAmount + tipAmount
+        self.totalBillAmount.text = "$\(totalAmount)"
+    }
+    
+    
+    @IBAction func twentyPercentTip(_ sender: Any) {
+        let totalBillAmount=Double(self.customerBillAmount.text ?? "") ?? 0
+        let tipPercent=0.20
+        let tipAmount = totalBillAmount * tipPercent
+        self.tipAmount.text = "$\(tipAmount)"
+        let totalAmount = totalBillAmount + tipAmount
+        self.totalBillAmount.text = "$\(totalAmount)"
+    }
+    
 }
+
 
